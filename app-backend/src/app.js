60b14be9ea92d93cbc84136eb73a763f6c021d9e
Dang Khoa <vodangkhoa835@gmail.com>
2023-03-12 18:44:43 +0700

    bug cors

--- app-backend/src/app.js
+++ app-backend/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const compression = require('compression');
 const { default : helmet} = require('helmet');
+var cors = require('cors');
 const app = express();
 
 
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
+app.use(cors()); 
 
 //init db
 require('./dbs/init.mongodb')
