d05168e95ad50477af02b6c001e21efa5f04f03b
trunghoai123 <trunghoaiitiuh@gmail.com>
2023-03-12 23:46:40 +0700

    added cors library (backend)

--- app-backend/src/app.js
+++ app-backend/src/app.js
@@ -1,26 +1,28 @@
-require('dotenv').config()
-const express = require('express');
-const morgan = require('morgan');
-const compression = require('compression');
-const { default : helmet} = require('helmet');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const { default: helmet } = require("helmet");
 const app = express();
-
+const cors = require("cors");
 
 //init middleware
-app.use(morgan("dev"))
-app.use(helmet())
-app.use(compression())
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
-
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
 //init db
-require('./dbs/init.mongodb')
+require("./dbs/init.mongodb");
 // const { checkOverload } = require("./helpers/check.connect")
 // checkOverload()
 
 //init routes
-app.use('', require('./routes'))
+app.use("", require("./routes"));
 
 module.exports = app;
