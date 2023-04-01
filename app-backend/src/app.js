27a1348889c0fe86a9e3ba63d83931ccaabd9fe4
Dang Khoa <vodangkhoa835@gmail.com>
2023-04-01 10:31:20 +0700

    fix file large

--- app-backend/src/app.js
+++ app-backend/src/app.js
@@ -10,10 +10,11 @@ const app = express();
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(
   express.urlencoded({
     extended: true,
+    limit: '50mb'
   })
 );
 app.use(cors());
